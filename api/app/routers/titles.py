27ac5d226f500a8b58390ccb43c3e4ca12975b9e
Sergio Diaz Ruiz <sergiodiazdev@gmail.com>
2025-07-04 16:58:34 +0200

    FIX titles

--- api/app/routers/titles.py
+++ api/app/routers/titles.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.db import SessionLocal
 from app.models import TitleBasic
 from app.db import get_db
 
 router = APIRouter(prefix="/titles", tags=["Titles"])
 
-
 @router.get("/{tconst}")
 def get_title(tconst: str, db: Session = Depends(get_db)):
-	return db.query(TitleBasic).filter(TitleBasic.tconst == tconst).first()
+	title = db.query(TitleBasic).filter(TitleBasic.tconst == tconst).first()
+	if not title:
+		raise HTTPException(status_code=404, detail="Title not found")
+	return title
